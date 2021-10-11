50c1c709a3bd5867128b6db98d4a2cac3b859c3e
Julian <thiele.juli@gmail.com>
2021-10-11 19:53:15 +0200

    added development mode

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import vuetify from "./plugins/vuetify";
 import VueSocketIOExt from "vue-socket.io-extended";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000", {
+const socket = io(process.env.VUE_APP_SOCKET_URL, {
   reconnectionDelayMax: 10000,
 });
 
